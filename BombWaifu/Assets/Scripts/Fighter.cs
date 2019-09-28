042cc3bd85e9c4430600fc70dd3008c6967dfffc
Kyle Townsend <townsend.kyle.kt@gmail.com>
2019-09-28 13:00:12 -0700

    added abstract class

---
+++ BombWaifu/Assets/Scripts/Fighter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Fighter
+{
+    float health;
+    int baseAttack;
+    float attackModifier;
+
+    public void Attack(float dmg, Fighter opponent) {
+        opponent.TakeDamage(dmg);
+    }
+
+    public void TakeDamage(float dmgTaken)
+    {
+        health -= dmgTaken;
+    }
+
+    public void ModifyAttack(float attackModchange)
+    {
+        attackModifier = attackModchange;
+    }
+}
