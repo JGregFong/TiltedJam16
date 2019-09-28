050ef443e60cdfad154a6abbe9f492b595f33978
Thana Sithisombath <ThanaSithisombath@gmail.com>
2019-09-28 14:08:29 -0700

    add FightUI & healthBar script.

---
+++ BombWaifu/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    float percentage = 1.0f;
+    GameObject healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = this.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.transform.localScale = new Vector3(percentage, 1.0f, 1.0f);
+    }
+
+    public void SetPercentage(float currentHealth)
+    {
+        percentage = currentHealth / 100;
+    }
+}
